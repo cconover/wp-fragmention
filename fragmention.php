48211dcb82b1c5ac741bebab091da6e9d4aaa64e
cconover <dev@cconover.com>
2014-04-22 20:29:59 +0000

    Initial commit

---
+++ fragmention.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Plugin Name: Fragmention
+ * Plugin URI: https://christiaanconover.com/code/wp-fragmention?ref=plugin-data
+ * Description: Add support for Fragmention links to WordPress.
+ * Version: 0.1.0
+ * Author: Christiaan Conover
+ * Author URI: https://christiaanconover.com?ref=wp-fragmention-plugin-author-uri
+ * License: GPLv2
+ * @package cconover
+ * @subpackage fragmention
+ **/
+ 
+ namespace cconover;
+
+/**
+ * Main plugin class
+ */
+class Fragmention {
+	// Plugin constants
+	const ID = 'cc-fragmention'; // Plugin ID
+	const NAME = 'Fragmention'; // Plugin name
+	const VERSION = '0.1.0'; // Plugin version
+	const WPVER = '2.7'; // Minimum version of WordPress required for this plugin
+	const PREFIX = 'cc_fragmention_'; // Plugin database/method prefix
+	
+	// Class properties
+	private $options; // Plugin options and settings
+	
+	// Class constructor
+	function __construct() {
+		// Add the script
+		add_action( 'wp_enqueue_scripts', array( &$this, 'loadscript' ) );
+		
+		// Admin
+		if ( is_admin() ) {
+			// Admin initialization
+			$this->admin_initialize();
+			
+			// Activation and deactivation hooks
+			register_activation_hook( __FILE__, array( &$this, 'activate' ) ); // Plugin activation
+			register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) ); // Plugin deactivation
+		}
+	} // End __construct()
+	
+	/* ===== Add Script ===== */
+	// Load the Fragmention script
+	function loadscript() {
+		// Use the WordPress hook to add the script inside <head>
+		wp_enqueue_script(
+			self::ID, // Handle for the script
+			plugins_url( 'fragmention.js', __FILE__ ), // Path to the script
+			array(), // Script dependencies
+			self::VERSION // Script version
+		);
+	} // End loadscript()
+	
+	/* ===== Admin Initialization ===== */
+	function admin_initialize() {
+		// Get plugin options from database
+		$this->options = get_option( self::PREFIX . 'options' );
+		
+		// Run upgrade process
+		$this->upgrade();
+	} // End admin_initialize()
+	
+	// Plugin upgrade
+	function upgrade() {
+		// Check whether the database-stored plugin version number is less than the current plugin version number, or whether there is no plugin version saved in the database
+		if ( ! empty( $this->options['dbversion'] ) && version_compare( $this->options['dbversion'], self::VERSION, '<' ) ) {
+			// Set local variable for options (always the first step in the upgrade process)
+			$options = $this->options;
+			
+			/* Update the plugin version saved in the database (always the last step of the upgrade process) */
+			// Set the value of the plugin version
+			$options['dbversion'] = self::VERSION;
+			
+			// Save to the database
+			update_option( self::PREFIX . 'options', $options );
+			/* End update plugin version */
+		}
+	} // End upgrade()
+	/*
+	===== End Admin Initialization =====
+	*/
+	
+	/*
+	===== Plugin Activation and Deactivation =====
+	*/
+	// Plugin activation
+	public function activate() {
+		// Check to make sure the version of WordPress being used is compatible with the plugin
+		if ( version_compare( get_bloginfo( 'version' ), self::WPVER, '<' ) ) {
+	 		wp_die( 'Your version of WordPress is too old to use this plugin. Please upgrade to the latest version of WordPress.' );
+	 	}
+	 	
+	 	// Default plugin options
+	 	$options = array(
+	 		'dbversion' => self::VERSION, // Current plugin version
+	 	);
+	 	
+	 	// Add options to database
+	 	add_option( self::PREFIX . 'options', $options );
+	} // End activate()
+	
+	// Plugin deactivation
+	public function deactivate() {
+		// Remove the plugin options from the database
+		delete_option( self::PREFIX . 'options' );
+	} // End deactivate
+	
+	/* ===== End Plugin Activation and Deactivation ===== */
+} // End main plugin class
+
+// Create plugin object
+$cc_fragmention = new \cconover\Fragmention;
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Fragmention ===
+Contributors: cconover
+Donate link: https://christiaanconover.com/code/wp-fragmention
+Tags: fragmention, link, javascript
+Requires at least: 2.7
+Tested up to: 3.9
+Stable tag: 0.1.0
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Add support for [Fragmention](https://medium.com/p/41ef2be9953f) links to WordPress.
+
+== Description ==
+
+Fragmentions for WordPress is a plugin to add Jonathan Neal's [Fragmentions script](https://github.com/chapmanu/fragmentions) to WordPress.
+
+== Installation ==
+
+1. Upload the `fragmention` directory to your site's `plugins` directory
+2. Activate the plugin through the `Plugins` menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Are there any plugin options? =
+Nope. Just activate the plugin and go. No configuration needed.
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+Initial release.
+
+== Changelog ==
+
+= 0.1.0 =
+Initial release.
